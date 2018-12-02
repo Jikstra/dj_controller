29b2ff52cfa769af0b494c549d9a178070c4363d
jikstra <jikstra@disroot.org>
2018-12-02 13:55:18 +0100

    Refactor prototype 2, use DEBUG flag, implement Button press recognition

--- prototyp 2/proto2/proto2.ino
+++ prototyp 2/proto2/proto2.ino
@@ -5,6 +5,8 @@
 #include <midi_Namespace.h>
 #include <midi_Settings.h>
 
+const bool DEBUG = true;
+
 
 Rotary rotaries[] = {
   Rotary(1, 2),
@@ -16,6 +18,21 @@ const int count_rotaries = 3;
 
 int counters[count_rotaries] = {0};
 
+struct Button {
+ int pin; 
+ bool is_pressed;
+ Button(int pin) : pin(pin), is_pressed(false) {}
+};
+
+
+Button buttons[] = {
+  { 12 }
+};
+
+
+
+const int count_buttons = 1;
+
 //int startTime;
 //int endTime;
 
@@ -24,30 +41,53 @@ MIDI_CREATE_INSTANCE(HardwareSerial,Serial, midiOut); // create a MIDI object ca
 
 void setup() {
   Serial.begin(9600);
+  pinMode(12, INPUT_PULLUP);
 }
 
 void loop() {
   //startTime = millis();
+
+
   int i;
   for(i = 0; i < count_rotaries; i++) {
     int result = rotaries[i].process();
+    
     if (result == DIR_NONE) {
       continue;
     }
-
+    
     if (result == DIR_CW) {
       counters[i]++;
     } else {
       counters[i]--;
     }
     
-    log(i);
+    rotaryTurned(i);
+  }
+
+  for(i = 0; i < count_buttons; i++) {
+    int button = digitalRead(buttons[i].pin);
+    if(button == LOW && buttons[i].is_pressed == false) {
+      Serial.println("Pressed!");
+      buttons[i].is_pressed = true;
+    } else if(button == HIGH && buttons[i].is_pressed == true) {
+      Serial.println("Released!");
+      buttons[i].is_pressed = false;
+    }
   }
   //endTime = millis();
   //Serial.println(endTime - startTime);
 
 }
 
-void log(int i) {
-  midiOut.sendControlChange(i,counters[i],1); // send a MIDI CC -- 56 = note, 127 = velocity, 1 = channel
+void buttonChanged(int i) {
+  
+}
+
+void rotaryTurned(int i) {
+  if(DEBUG == false) {
+    midiOut.sendControlChange(i,counters[i],1); // send a MIDI CC -- 56 = note, 127 = velocity, 1 = channel
+  } else {
+    Serial.println(String(i) + " " + counters[i]);  
+  }
 }
