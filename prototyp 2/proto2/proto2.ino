ca7e8e94351c3d3727c1dfb5f4e9ef4545ff0e46
jikstra <jikstra@disroot.org>
2018-12-02 15:12:37 +0100

    Refactor, no we're easily able to add new knobs and set their input pins/controller_number/channel

--- prototyp 2/proto2/proto2.ino
+++ prototyp 2/proto2/proto2.ino
@@ -4,34 +4,33 @@
 #include <midi_Message.h>
 #include <midi_Namespace.h>
 #include <midi_Settings.h>
+#include <stdarg.h>
 
 const bool DEBUG = true;
 
 
-Rotary rotaries[] = {
-  Rotary(1, 2),
-  Rotary(3, 4),
-  Rotary(5, 6),
+struct Knob {
+  int control_number;
+  int channel;
+  int button_pin;
+  bool button_is_pressed;  
+  Rotary rotary;
+  int rotary_counter;
+  Knob(int rotary_pin_a, int rotary_pin_b, int button_pin, int control_number, int channel) : 
+    button_pin(button_pin),
+    button_is_pressed(false),
+    rotary(Rotary(rotary_pin_a, rotary_pin_b)),
+    rotary_counter(0),
+    control_number(control_number),
+    channel(channel) {}
 };
 
-const int count_rotaries = 3;
-
-int counters[count_rotaries] = {0};
-
-struct Button {
- int pin; 
- bool is_pressed;
- Button(int pin) : pin(pin), is_pressed(false) {}
+Knob knobs[] = {
+  {9, 10, 11, 1, 1},
+  {1, 2, 3, 2, 1},
 };
 
-
-Button buttons[] = {
-  { 12 }
-};
-
-
-
-const int count_buttons = 1;
+int count_knobs = 2;
 
 //int startTime;
 //int endTime;
@@ -41,53 +40,77 @@ MIDI_CREATE_INSTANCE(HardwareSerial,Serial, midiOut); // create a MIDI object ca
 
 void setup() {
   Serial.begin(9600);
-  pinMode(12, INPUT_PULLUP);
+  for(int i = 0; i < count_knobs; i++) {
+    Knob* knob = &knobs[i];
+    
+    pinMode(knob->button_pin, INPUT_PULLUP);
+  }
+  
 }
 
 void loop() {
   //startTime = millis();
 
-
   int i;
-  for(i = 0; i < count_rotaries; i++) {
-    int result = rotaries[i].process();
+  for(i = 0; i < count_knobs; i++) {
+    // Rotation
+    Knob* knob = &knobs[i];
     
-    if (result == DIR_NONE) {
-      continue;
-    }
-    
-    if (result == DIR_CW) {
-      counters[i]++;
-    } else {
-      counters[i]--;
-    }
-    
-    rotaryTurned(i);
-  }
-
-  for(i = 0; i < count_buttons; i++) {
-    int button = digitalRead(buttons[i].pin);
-    if(button == LOW && buttons[i].is_pressed == false) {
-      Serial.println("Pressed!");
-      buttons[i].is_pressed = true;
-    } else if(button == HIGH && buttons[i].is_pressed == true) {
-      Serial.println("Released!");
-      buttons[i].is_pressed = false;
-    }
+    knobProcessRotary(knob);
+    knobProcessButton(knob);
   }
   //endTime = millis();
   //Serial.println(endTime - startTime);
 
 }
 
-void buttonChanged(int i) {
+void knobProcessRotary(Knob* knob) {
+  int result = knob->rotary.process();
+
+  if (result == DIR_NONE) return;
+
+  if (result == DIR_CW) {
+    knob->rotary_counter++;
+  } else {
+    knob->rotary_counter--;
+  }
   
+  if(DEBUG == false) {
+    // send a MIDI CC -- 56 = note, 127 = velocity, 1 = channel
+    midiOut.sendControlChange(
+      knob->control_number,
+      knob->rotary_counter,
+      knob->channel
+    );
+  } else {
+    p("Rotation: %i:%i %i", knob->control_number, knob->channel, knob->rotary_counter);  
+  }
 }
 
-void rotaryTurned(int i) {
-  if(DEBUG == false) {
-    midiOut.sendControlChange(i,counters[i],1); // send a MIDI CC -- 56 = note, 127 = velocity, 1 = channel
+void knobProcessButton(Knob* knob) {
+  int button = digitalRead(knob->button_pin);
+  
+  if(button == LOW && knob->button_is_pressed == false) {
+    knob->button_is_pressed = true;
+  } else if(button == HIGH && knob->button_is_pressed == true) {
+    knob->button_is_pressed = false;
   } else {
-    Serial.println(String(i) + " " + counters[i]);  
+    return;
   }
+
+  if(DEBUG == false) {
+    // TODO: Implement midi 
+  } else {
+    p("Button: %i:%i %s", knob->control_number, knob->channel, knob->button_is_pressed ? "Pressed" : "Released");
+  }
+}
+
+
+void p(char *fmt, ... ){
+  char buf[128]; // resulting string limited to 128 chars
+  va_list args;
+  va_start (args, fmt );
+  vsnprintf(buf, 128, fmt, args);
+  va_end (args);
+  Serial.println(buf);
 }
